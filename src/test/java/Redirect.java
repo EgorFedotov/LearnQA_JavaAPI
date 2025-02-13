9324849b66ccf8aea3078ce4f3a77d26f14b05c6
egorf <egorfedotovarz@gmail.com>
2025-02-13 18:02:10 +0300

    ex7 long redirect

--- src/test/java/Redirect.java
+++ src/test/java/Redirect.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -6,12 +8,31 @@ public class Redirect {
 
     @Test
     public void GetRedirect(){
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        boolean flag = true;
+
+        while (flag){
+            Response response = GetRequest(url);
+            url = response.getHeader("Location");
+            if(url == null){
+                System.out.println("Редиректа нет");
+                break;
+            }
+            System.out.println("сейчас редиректит вот на этот адресс" + url + " стаус код" + response.statusCode());
+            if(response.statusCode()==200){
+                flag = false;
+            }
+        }
+    }
+
+    public Response GetRequest(String url){
         Response response = RestAssured
                 .given()
-                .get("https://playground.learnqa.ru/api/long_redirect")
+                .redirects()
+                .follow(false)
+                .when()
+                .get(url)
                 .andReturn();
-
-        String redirectAddress = response.getHeader("x-host");
-        System.out.println("redirect address " + "\n"+ redirectAddress);
+        return response;
     }
 }
