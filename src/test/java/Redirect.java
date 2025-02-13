378e9a637a10700c3afe8a10cded88e9cbc6c3ed
egorf <egorfedotovarz@gmail.com>
2025-02-13 17:02:26 +0300

    ex6 redirect

---
+++ src/test/java/Redirect.java
@@ -0,0 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Redirect {
+
+    @Test
+    public void GetRedirect(){
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String redirectAddress = response.getHeader("x-host");
+        System.out.println("redirect address " + "\n"+ response.getHeader("x-host"));
+    }
+}
