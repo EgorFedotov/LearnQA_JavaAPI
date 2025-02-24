0e8854a90556b1b3f5050e0712868317d5991f57
egorf <egorfedotovarz@gmail.com>
2025-02-24 14:14:42 +0300

    test length of the phrase

---
+++ src/test/java/tests/ShortPhraseTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShortPhraseTest {
+
+    String phrase = "Hello, world";
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Hello, world", "this is too long a phrase for the test", ""})
+    public void testLengthOfPhrase(String phrase){
+        assertTrue(phrase.length()>15, "The phrase should be greater then 15, length phrase - " + phrase.length());
+    }
+}
