e3e6cbc8f2fc080320847800941fca2723b3771b
egorf <egorfedotovarz@gmail.com>
2025-02-26 11:46:24 +0300

    test create user which already exists

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -12,5 +12,21 @@ public class Assertions {
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectValue, value, "Json value is bor equal to expected value");
     }
+
+    public static void assertResponseTextEquals (Response Response, String expectedAnswer){
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals (Response Response, int expectedStatusCode){
+        assertEquals(
+                expectedStatusCode,
+                Response.statusCode(),
+                "Response code is not as expected"
+        );
+    }
 }
 
